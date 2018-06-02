6cfd1aafb0631f1278cc75e5f82df1f388d97c0b
zhoutao <zhoutao825638@vip.qq.com>
2018-06-02 12:15:39 +0800

    doc:添加注释信息

--- src/main/java/com/tao/example/CalculationService.java
+++ src/main/java/com/tao/example/CalculationService.java
@@ -6,7 +6,6 @@ import com.tao.example.grpc.basic.GrpcResponse;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import io.grpc.stub.StreamObserver;
-
 import java.io.IOException;
 import java.util.Optional;
 import java.util.logging.Level;
@@ -43,31 +42,46 @@ public class CalculationService {
     Optional.of(server).map(s -> s.shutdown()).orElse(null);
   }
 
+  /**
+   * 循环运行服务,封锁停止
+   *
+   * @throws InterruptedException
+   */
   public void blockUnitShutdown() throws InterruptedException {
     if (server != null) {
       server.awaitTermination();
     }
   }
 
+  /**
+   * 程序的主运行窗口
+   *
+   * @param args
+   * @throws IOException
+   * @throws InterruptedException
+   */
   public static void main(String[] args) throws IOException, InterruptedException {
     CalculationService service = new CalculationService();
     service.start(SERVER_PORT);
     service.blockUnitShutdown();
   }
 
+  /** 实现的服务类 */
   static class BasicCalImpl extends GrpcGrpc.GrpcImplBase {
 
     @Override
     public void calculation(GrpcRequest request, StreamObserver<GrpcResponse> responseObserver) {
+      // 获取数据信息
       int num1 = Integer.parseInt(request.getNum1());
       int num2 = Integer.parseInt(request.getNum2());
-
+      // 计算数据
       GrpcResponse response =
           GrpcResponse.newBuilder()
               .setSum(String.valueOf(num1 + num2))
               .setSub(String.valueOf(num1 - num2))
               .setProduct(String.valueOf(num1 * num2))
               .build();
+      // 返回数据，完成此次请求
       responseObserver.onNext(response);
       responseObserver.onCompleted();
     }
