381a24e7871844bcc8249b332ab7beae27e8bd8b
zhoutao <zhoutao825638@vip.qq.com>
2018-06-02 11:20:58 +0800

    feat:完成客户端的代码编写

--- src/main/java/com/tao/example/CalculationClient.java
+++ src/main/java/com/tao/example/CalculationClient.java
@@ -3,64 +3,73 @@ package com.tao.example;
 import com.tao.example.grpc.basic.GrpcGrpc;
 import com.tao.example.grpc.basic.GrpcRequest;
 import com.tao.example.grpc.basic.GrpcResponse;
-import io.grpc.Server;
-import io.grpc.ServerBuilder;
-import io.grpc.stub.StreamObserver;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
 
 import java.io.IOException;
-import java.util.Optional;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.regex.Pattern;
+
+import static com.tao.example.CalculationService.SERVER_PORT;
 
 public class CalculationClient {
   private static final Logger logger = Logger.getLogger(CalculationClient.class.getName());
 
-  private Server server;
+  private ManagedChannel managedChannel;
 
-  private void start(int port) throws IOException {
-    server = ServerBuilder.forPort(port).addService(new BasicCalImpl()).build().start();
-    logger.log(Level.WARNING, "服务已经启动,监听端口：" + port);
-    Runtime.getRuntime()
-        .addShutdownHook(
-            new Thread(
-                () -> {
-                  logger.log(Level.WARNING, "监听到JVM停止,正在关闭GRPC服务....");
-                  CalculationClient.this.stop();
-                  logger.log(Level.WARNING, "服务已经停止...");
-                }));
+  private GrpcGrpc.GrpcBlockingStub blockingStub;
+
+  public CalculationClient(String host, int port) {
+    this(ManagedChannelBuilder.forAddress(host, port).usePlaintext(true));
   }
 
-  public void stop() {
-    Optional.of(server).map(s -> s.shutdown()).orElse(null);
-  }
-
-  public void blockUnitShudown() throws InterruptedException {
-    if (server != null) {
-      server.awaitTermination();
+  public void sendMessage(String num1, String num2) {
+    logger.log(Level.INFO, "尝试发送: num1 = " + num1 + ",num2 = " + num2);
+    GrpcRequest request = GrpcRequest.newBuilder().setNum1(num1).setNum2(num2).build();
+    GrpcResponse response = null;
+    try {
+      response = blockingStub.calculation(request);
+      System.out.println("两数的和 = " + response.getSum());
+      System.out.println("两数的差 = " + response.getSub());
+      System.out.println("两数的积 = " + response.getProduct());
+    } catch (StatusRuntimeException ex) {
+      logger.log(Level.WARNING, "发送消息出现异常", ex);
     }
+  }
+
+  /**
+   * 关闭客户端
+   *
+   * @throws InterruptedException
+   */
+  public void shutdown() throws InterruptedException {
+    managedChannel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+  }
+
+  CalculationClient(ManagedChannelBuilder<?> channelBuilder) {
+    managedChannel = channelBuilder.build();
+    blockingStub = GrpcGrpc.newBlockingStub(managedChannel);
   }
 
   public static void main(String[] args) throws IOException, InterruptedException {
-    CalculationClient service  = new CalculationClient();
-    service.start(8888);
-    service.blockUnitShudown();
-  }
-
-  static class BasicCalImpl extends GrpcGrpc.GrpcImplBase {
-
-    @Override
-    public void calculation(GrpcRequest request, StreamObserver<GrpcResponse> responseObserver) {
-      int num1 = Integer.parseInt(request.getNum1());
-      int num2 = Integer.parseInt(request.getNum2());
-
-      GrpcResponse response =
-          GrpcResponse.newBuilder()
-              .setSub(String.valueOf(num1 + num2))
-              .setSub(String.valueOf(num1 - num2))
-              .setProduct(String.valueOf(num1 * num2))
-              .build();
-      responseObserver.onNext(response);
-      responseObserver.onCompleted();
+    String host = "127.0.0.1";
+    CalculationClient client = new CalculationClient(host, SERVER_PORT);
+    Scanner scanner = new Scanner(System.in);
+    Pattern pattern = Pattern.compile("^[-\\+]?[\\d]*$");
+    System.out.print("请输入Num1:");
+    String num1Str = scanner.next();
+    if (!pattern.matcher(num1Str).matches()) {
+      logger.log(Level.WARNING, "num1不是一个整数,程序无法运行");
     }
+    System.out.print("请输入Num2:");
+    String num2Str = scanner.next();
+    if (!pattern.matcher(num2Str).matches()) {
+      logger.log(Level.WARNING, "num2不是一个整数,程序无法运行");
+    }
+    client.sendMessage(num1Str, num2Str);
   }
 }
